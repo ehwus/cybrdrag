0850ad8d6d2edcdf1ffdebcc1ae80b31ec32fcc4
Alex Withington-Smith <alex@awithsmith.com>
2021-01-18 15:53:25 +0000

    linked mongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
 
 require('dotenv').config();
 
@@ -8,6 +9,14 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+const uri = process.env.ATLAS_URI;
+
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true });
+const connection = mongoose.connection;
+
+connection.once('open', () => {
+    console.log("MongoDB DB connection established")
+})
 
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
