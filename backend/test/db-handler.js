f6e0b2a0ab36706a7e2c22fae64244356f67e858
alexleesonmill <alexleesonmill@gmail.com>
2021-01-19 12:48:57 +0000

    Test - added test for ensuring the db dropped between tests

---
+++ backend/test/db-handler.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+const { MongoMemoryServer } = require('mongodb-memory-server');
+
+const mongod = new MongoMemoryServer();
+
+module.exports.connect = async () => {
+    const uri = await mongod.getConnectionString();
+
+    const mongooseOpts = {
+        useNewUrlParser: true,
+        autoReconnect: true,
+        reconnectTries: Number.MAX_VALUE,
+        reconnectInterval: 1000
+    };
+
+    await mongoose.connect(uri, mongooseOpts);
+}
+
+module.exports.closeDatabase = async () => {
+    await mongoose.connection.dropDatabase();
+    await mongoose.connection.close();
+    await mongod.stop();
+}
+
+module.exports.clearDatabase = async () => {
+    await mongoose.connection.dropDatabase();
+}
---
+++ backend/test/performer.test.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+
+const dbHandler = require('./db-handler');
+const performerService = require('../src/services/performer');
+const performerModel = require('../src/models/performer');
+
+beforeAll(async () => await dbHandler.connect());
+
+afterEach(async () => await dbHandler.clearDatabase());
+
+afterAll(async () => await dbHandler.closeDatabase());
+
+describe('performer ', () => {
+    it('can be created correctly', async () => {
+        await expect(performerService.create(performerComplete))
+            .resolves
+            .not
+            .toThrow();
+    });
+});
+
+describe('tester ', () => {
+    it('creates with the correct name', async () => {
+        await performerService.create(performerIncomplete);
+        await performerModel.find().lean().then(console.log);
+    });
+});
+
+const performerComplete = {
+    name: 'Moxy Bowels',
+    netWorth: 5000,
+};
+
+const performerIncomplete = {
+    name: 'Boxy Mowels',
+    netWorth: 5,
+}
