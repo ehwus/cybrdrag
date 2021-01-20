577e316c21a077d42f787608d3ec113f325ba148
Alex Withington-Smith <alex@awithsmith.com>
2021-01-20 14:40:46 +0000

    added test for a token on a successful signup

--- tests/users.test.js
+++ tests/users.test.js
@@ -1,17 +1,35 @@
 const app = require('../server');
+const User = require('../models/User');
 const supertest = require('supertest');
 const request = supertest(app);
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
 const dbHandler = require('./db-handler');
 
 beforeAll(async () => await dbHandler.connect());
-
 afterEach(async () => await dbHandler.clearDatabase());
 afterAll(async () => await dbHandler.closeDatabase());
 
-describe('testing', () => {
-  it('Connects to test', async (done) => {
-    const res = await request.get('/');
-    expect(res.status).toBe(200);
-    done();
+describe('Users', () => {
+  it('Empty post request gives an array of three errors', async () => {
+    const createUser = await request.post('/api/users');
+    let errors = createUser.body.errors.map((e) => e.msg);
+    expect(errors.length).toEqual(3);
+    expect(errors[0]).toEqual('Usernames must be between 5 and 30 characters');
+    expect(errors[1]).toEqual('Please include a valid email');
+    expect(errors[2]).toEqual('Password must be at least 5 characters long');
+  });
+
+  it('Returns a JSON token on successful sign up', async () => {
+    const createUser = await request
+      .post('/api/users')
+      .send({
+        username: 'kenneth',
+        email: 'kenneth@biz.com',
+        password: 'partario',
+      })
+      .set('Accept', 'application/json');
+
+    expect(createUser.body.token).not.toBe(null);
   });
 });
