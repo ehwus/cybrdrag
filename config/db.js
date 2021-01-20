205379e394ec188c3947e92aede911d2e44c57d3
Alex Withington-Smith <alex@awithsmith.com>
2021-01-20 10:10:00 +0000

    added mongodb connection

---
+++ config/db.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+// Connect to local if not production
+const db = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/cybrdrag';
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+    });
+    console.log('MongoDB Connected');
+  } catch (err) {
+    console.error(err.message);
+    // Exit with failure
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express');
 require('dotenv').config();
+const connectDB = require('./config/db');
 
 const app = express();
+
+connectDB();
 
 const PORT = process.env.PORT || 5000;
 
