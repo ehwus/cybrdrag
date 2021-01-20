12659a39dabc0aeae11b1dd5c4251f0e3585df1a
Alex Withington-Smith <alex@awithsmith.com>
2021-01-20 16:24:50 +0000

    prettier on everything

--- tests/auth.test.js
+++ tests/auth.test.js
@@ -14,17 +14,20 @@ describe('Auth', () => {
   it('Empty post request leads to the following errors', async () => {
     const createAuthEmptyUser = await request.post('/api/auth');
     let errors = createAuthEmptyUser.body.errors.map((e) => e.msg);
-    expect(errors.length).toEqual(2)
-    expect(errors).toContain('Please include a valid email')
-    expect(errors).toContain('Password is required')
+    expect(errors.length).toEqual(2);
+    expect(errors).toContain('Please include a valid email');
+    expect(errors).toContain('Password is required');
   });
 
   it('Returns a JSON token on successful sign in', async () => {
     const createUser = await createValidUser();
-    let authValidUser = await request.post('/api/auth').send({
-      email: 'kenneth@biz.com',
-      password: 'partario',
-    }).set('Accept', 'application/json');
-    expect(authValidUser.body.token).not.toBe(null)
+    let authValidUser = await request
+      .post('/api/auth')
+      .send({
+        email: 'kenneth@biz.com',
+        password: 'partario',
+      })
+      .set('Accept', 'application/json');
+    expect(authValidUser.body.token).not.toBe(null);
   });
 });
