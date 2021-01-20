3ddee5e8a775d83a6d51980230e3a40391f62c27
Alex Withington-Smith <alex@awithsmith.com>
2021-01-20 12:14:39 +0000

    added route to get a user from a valid token

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -21,7 +21,6 @@ router.post(
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-
     const { email, password } = req.body;
 
     try {
@@ -62,5 +61,18 @@ router.post(
     }
   }
 );
+
+// @route    GET api/auth
+// @desc     Get user from token
+// @access   Public
+router.get('/', auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select('-password');
+    res.json(user);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server error');
+  }
+});
 
 module.exports = router;
