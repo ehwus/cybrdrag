9ae17a3edd1086b109ab789c30916fab496db931
Michael Linford <linford.mike@gmail.com>
2021-01-20 16:40:48 +0000

    Auth tests now include checking for the correct username and passwords, and the associated correct responses. It also tests that a server error is returned when something goes wrong in login.

--- tests/auth.test.js
+++ tests/auth.test.js
@@ -14,20 +14,56 @@ describe('Auth', () => {
   it('Empty post request leads to the following errors', async () => {
     const createAuthEmptyUser = await request.post('/api/auth');
     let errors = createAuthEmptyUser.body.errors.map((e) => e.msg);
-    expect(errors.length).toEqual(2);
-    expect(errors).toContain('Please include a valid email');
-    expect(errors).toContain('Password is required');
+    expect(errors.length).toEqual(2)
+    expect(errors).toContain('Please include a valid email')
+    expect(errors).toContain('Password is required')
   });
 
   it('Returns a JSON token on successful sign in', async () => {
     const createUser = await createValidUser();
-    let authValidUser = await request
-      .post('/api/auth')
-      .send({
-        email: 'kenneth@biz.com',
-        password: 'partario',
-      })
-      .set('Accept', 'application/json');
-    expect(authValidUser.body.token).not.toBe(null);
+    let authValidUser = await request.post('/api/auth').send({
+      email: 'kenneth@biz.com',
+      password: 'partario',
+    }).set('Accept', 'application/json');
+    expect(authValidUser.body.token).not.toBe(null)
   });
+
+  it('Returns a status 400 with an error if theres not the correct username', async () => {
+    const createUser = await createValidUser();
+    let authValidUser = await request.post('/api/auth').send({
+      email: 'ken@biz.com',
+      password: 'partario',
+    }).set('Accept', 'application/json');
+    const error = authValidUser.body.errors.map((e) => e.msg);
+    expect(error).toContain('Invalid Credentials')
+  })
+
+  it('Returns a status 400 with an error if the password is incorrect', async () => {
+    const createUser = await createValidUser();
+    let authValidUser = await request.post('/api/auth').send({
+      email: 'kenneth@biz.com',
+      password: 'part',
+    }).set('Accept', 'application/json');
+    const error = authValidUser.body.errors.map((e) => e.msg);
+    expect(error).toContain('Invalid Credentials')
+  })
+
+  it('returns a server error if something goes wrong during the creation process', async () => {
+    const oldSecret = process.env.JWT_SECRET;
+    const oldConsole = console.error;
+    console.error = jest.fn();
+    process.env.JWT_SECRET = '';
+
+    await createValidUser();
+    let attemptLogin = await request.post('/api/auth').send({
+      email: 'kenneth@biz.com',
+      password: 'partario',
+    }).set('Accept', 'application/json');
+
+    expect(attemptLogin.status).toEqual(500);
+
+    process.env.JWT_SECRET = oldSecret;
+    console.error = oldConsole;
+  });
+
 });
