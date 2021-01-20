62dd85ce84f4266dfdd05561a6e57fb75f7e5db4
Alex Withington-Smith <alex@awithsmith.com>
2021-01-20 09:43:11 +0000

    blank slate

--- backend/src/models/performer.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const performerSchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    netWorth: { type: Number, required: true },
-});
-
-module.exports = mongoose.model('performer', performerSchema)
