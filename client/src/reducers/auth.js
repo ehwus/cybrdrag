3ae73850e7e22b9b777dc16db28dfb0575cf4a46
alexleesonmill <alexleesonmill@gmail.com>
2021-01-25 18:48:22 +0000

    Updated navbar to render properly when logged in/out

--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -1,4 +1,4 @@
-import { REGISTER_SUCCESS, REGISTER_FAIL, USER_LOADED, AUTH_ERROR, LOGIN_FAIL, LOGIN_SUCCESS } from "../actions/types";
+import { REGISTER_SUCCESS, REGISTER_FAIL, USER_LOADED, AUTH_ERROR, LOGIN_FAIL, LOGIN_SUCCESS, LOGOUT } from "../actions/types";
 
 const initialState = {
   token: localStorage.getItem('token'),
@@ -30,6 +30,7 @@ export default function (state = initialState, action) {
     case REGISTER_FAIL:
     case AUTH_ERROR:
     case LOGIN_FAIL:
+    case LOGOUT:
       localStorage.removeItem('token');
       return {
         ...state,
