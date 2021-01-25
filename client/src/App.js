93deaddfaef49c5aaac48aff74aa52a7f555a398
alexleesonmill <alexleesonmill@gmail.com>
2021-01-25 20:20:11 +0000

    Updated app, routing and views for homepage and dashboard, ready for content

--- client/src/App.js
+++ client/src/App.js
@@ -2,9 +2,11 @@ import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 import Navbar from './components/layout/Navbar';
-import Login from './components/layout/Login';
 import Alert from './components/layout/Alert';
-import Register from './components/layout/Register';
+import Landing from "./components/layout/Landing";
+import Routes from './components/routing/Routes';
+
+
 //redux
 import { Provider } from 'react-redux';
 import store from './store';
@@ -27,8 +29,8 @@ const App = () => {
       {/*We don't have a section container here, do we need one?*/}
       <Alert />
       <Switch>
-        <Route exact path='/login' component={Login} />
-        <Route exact path='/register' component={Register} />
+        <Route exact path='/' component={Landing} />
+        <Route component={Routes} />
       </Switch>
     </Router>
   </Provider>
