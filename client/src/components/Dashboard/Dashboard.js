d274220379281467b21fb7717b7c571cf3cd2a9a
Michael Linford <linford.mike@gmail.com>
2021-01-27 17:29:04 +0000

    Top 3 queens now displays on dashboard.

--- client/src/components/Dashboard/Dashboard.js
+++ client/src/components/Dashboard/Dashboard.js
@@ -1,22 +1,12 @@
 import React from 'react';
-import PerformerCard from '../performers/PerformerCard';
 // import PropTypes from 'prop-types';
 import ShowAllButton from '../Buttons/ShowAllButton';
-
-
-let testPerformer = {
-  name: `Testy O'Sterone`,
-  worth: 3456,
-  avatar: 'https://avatars.dicebear.com/4.5/api/female/dsfawerhtrs.svg',
-};
+import TopThreePerformers from '../performers/TopThreePerformers';
 
 const Dashboard = (props) => {
   return (
     <div className='container'>
-      <h1 className='authstate'>
-        Test Dashboard at components/dashboard/dashboard.js
-      </h1>
-      <PerformerCard performer={testPerformer} />
+    <TopThreePerformers/>
       <ShowAllButton />
     </div>
   );
