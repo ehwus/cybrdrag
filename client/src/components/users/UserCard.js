3874a4b6b41e981664a232422f74b97d7355ac9f
Alex Withington-Smith <alex@awithsmith.com>
2021-01-28 17:48:47 +0000

    added an excellent profile that i'm really happy with

--- client/src/components/users/UserCard.js
+++ client/src/components/users/UserCard.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-
 const UserCard = ({ user }) => {
-    return (
-      <div>
-        {user.username}
-        {user.balance}
+  return (
+    <div className='userCard'>
+      <h1 className='dashboardTitle'>PROFILE</h1>
+      <div className='username'>Username: {user.username}</div>
+      <div className='userBalance'>
+        Balance: {user.balance}
+        <i className='fas fa-dice-d20'></i>
       </div>
-    )
+    </div>
+  );
 };
 
-UserCard.propTypes ={
+UserCard.propTypes = {
   user: PropTypes.object.isRequired,
 };
 
