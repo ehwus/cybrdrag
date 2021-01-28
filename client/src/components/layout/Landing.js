d8c5e5a5b49cff822f575cece7f3ecd3fb792a51
Alex Withington-Smith <alex@awithsmith.com>
2021-01-28 10:20:47 +0000

    minor tweak to flavour text

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -14,7 +14,20 @@ const Landing = ({ isAuthenticated }) => {
         <p>DO DRAG QUEENS DREAM OF ELECTRIC SHEEP?</p>
         <p>NO.</p>
         <p>DRAG QUEENS DON'T SLEEP!</p>
-        <p>INVEST IN YOUR FAVOURITE PERFORMERS TODAY!</p>
+        <p className='landingDescription'>
+          Cybrdrag is the new streaming platform from jeffCorp, and you're
+          invited to be an angel investor.
+        </p>
+        <p className='landingDescription'>
+          Invest in performers that you love, and help them stave off a
+          mandatory labour reassignment to our Mars Colonies.
+        </p>
+        <p className='landingDescription'>
+          Nothing is free. jeffCoins get you shares, but invest wisely.
+        </p>
+        <p className='landingDescription'>
+          Queens perform on the hour every hour.
+        </p>
       </div>
     </div>
   );
