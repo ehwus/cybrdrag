6b4341498eeb0e3055f74ba28c1f17767c508afd
Alex Withington-Smith <alex@awithsmith.com>
2021-01-28 10:12:37 +0000

    added placeholder text to landing page

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -1,26 +1,31 @@
 import React from 'react';
-import {Link, Redirect} from 'react-router-dom';
-import {connect} from 'react-redux';
+import { Link, Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 
-const Landing = ({isAuthenticated}) => {
+const Landing = ({ isAuthenticated }) => {
   if (isAuthenticated) {
-    return <Redirect to='/dashboard'/>;
+    return <Redirect to='/dashboard' />;
   }
 
   return (
     <div className='container'>
-      <h1 className='authstate'>Test Landing Page found at /components/layout/Landing.js</h1>
+      <div className='landingBox'>
+        <p>DO DRAG QUEENS DREAM OF ELECTRIC SHEEP?</p>
+        <p>NO.</p>
+        <p>DRAG QUEENS DON'T SLEEP!</p>
+        <p>INVEST IN YOUR FAVOURITE PERFORMERS TODAY!</p>
+      </div>
     </div>
   );
 };
 
 Landing.propTypes = {
-  isAuthenticated: PropTypes.bool
+  isAuthenticated: PropTypes.bool,
 };
 
-const mapStateToProps = state => ({
-  isAuthenticated: state.auth.isAuthenticated
+const mapStateToProps = (state) => ({
+  isAuthenticated: state.auth.isAuthenticated,
 });
 
 export default connect(mapStateToProps)(Landing);
