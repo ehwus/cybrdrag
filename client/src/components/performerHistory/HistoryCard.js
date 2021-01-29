629c6d0786fbcb61f45c950035f7ad0caa36ffb9
Alex Withington-Smith <alex@awithsmith.com>
2021-01-29 07:30:23 +0000

    added formatting to time

--- client/src/components/performerHistory/HistoryCard.js
+++ client/src/components/performerHistory/HistoryCard.js
@@ -1,15 +1,21 @@
 import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+import Moment from 'react-moment';
 
 const HistoryCard = (performances) => {
+  let date = new Date(Date.parse(performances.performances.date));
+
   return (
     <div className='historyCard'>
+      <div className='performanceTime'>
+        Performance at &nbsp;
+        <Moment date={date} format='HH:MM on DD/MM/YY' />
+      </div>
       <h5>
-        Performance earnings: {performances.performances.netearned}
+        Earned: {performances.performances.netearned}
         <i className='fas fa-dice-d20'></i>
       </h5>
-      <h5>Performance time: {performances.performances.date}</h5>
     </div>
   );
 };
